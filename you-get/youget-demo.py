33a5e2873e13d6849bb8d77012b3e239e774c2d1
xdx <16348104@qq.com>
2021-07-11 17:24:29 +0800

    you-get 下载音频，视频

--- you-get/youget-demo.py
+++ you-get/youget-demo.py
@@ -4,7 +4,7 @@ from you_get import common as you_get #导入you-get库
 # 设置下载目录mac
 directory = '/Users/xdx/Downloads/pdf/'
 #需要下载的视频地址,部分网址不能用
-url = 'https://www.bilibili.com/video/BV1Vt411579n?from=search&seid=17215681599528404474'
+url = 'https://music.163.com/#/song?id=25638324'
 #sys传递参数执行下载，就像在命令行一样；‘-o’后面跟保存目录。
 sys.argv = ['you-get', '-o', directory, url]
 you_get.main()
